aea1d7347209c9e0ed1774ff2a18e88064429919
Gustavo <gustavo.gst15@gmail.com>
2025-04-03 23:19:15 -0300

    fix: remove prop from state

--- src/States/game_state.pl
+++ src/States/game_state.pl
@@ -20,14 +20,13 @@ make_row(I, Row) :-
 
 make_cell(I, J, Cell) :-
     (   I >= 5, (I + J) mod 2 =:= 0 -> make_cell_with_player(p1, Cell)
-    ;   I =:= 7, J =:= 0 -> make_cell_with_cursor(true, Cell)
+    ;   I =:= 7, J =:= 0 -> make_empty_cell(Cell)
     ;   I =< 2, (I + J) mod 2 =:= 0 -> make_cell_with_player(p2, Cell)
-    ;   make_cell_with_cursor(false, Cell)
+    ;   make_empty_cell(Cell)
     ).
 
-make_cell_with_cursor(IsUnderCursor, Cell) :-
+make_empty_cell(Cell) :-
     Cell = cell{
-        is_under_cursor: IsUnderCursor,
         is_selected: false,
         is_available: false,
         player: none,
@@ -36,7 +35,6 @@ make_cell_with_cursor(IsUnderCursor, Cell) :-
 
 make_cell_with_player(Player, Cell) :-
     Cell = cell{
-        is_under_cursor: false,
         is_selected: false,
         is_available: false,
         player: Player,
